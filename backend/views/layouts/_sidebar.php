b1a3dd19b398c042d49ba205477727c0cc002308
HoangAnh1253 <just4codee@gmail.con>
2023-02-21 17:47:40 +0700

    done sidebar for backend

--- backend/views/layouts/_sidebar.php
+++ backend/views/layouts/_sidebar.php
@@ -1,16 +1,25 @@
 <?php
-    echo \yii\bootstrap5\Nav::widget([
+
+?>
+
+<aside>
+    <?php
+        echo \yii\bootstrap5\Nav::widget([
             'options' => [
-                    'class' => 'd-flex flex-column nav-pills'
+                'class' => 'd-flex flex-column nav-pills'
             ],
             'items' => [
-                    [
-                        'label' => 'Dashboard',
-                        'url' => ['/site/index'
-                    ]
-            ]
-    ]]);
-?>
+                [
+                    'label' => 'Dashboard',
+                    'url' => ['/site/index']
+                ],
+                [
+                    'label' => 'Videos',
+                    'url' => ['/video/index']
+                ]
+            ]]);
+    ?>
+</aside>
 
 <!---->
 <!--<div class="list-group">-->
